e3023c1387dac4505f4895f1a429fc9ebe2f8f96
Dnyaneshwar Belhekar <priyanka.belhekar@accenture.com>
2022-11-27 21:46:37 +0530

    feat(tests): init unit tests

---
+++ src/test/Loader.test.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable import/named */
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import Loader from '../components/Loader';
+
+describe('Loader', () => {
+  it('renders loader', () => {
+    render(<Loader />);
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+    screen.debug();
+  });
+});
